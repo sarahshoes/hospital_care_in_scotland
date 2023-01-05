bf157fb5ce60cc5f7a1d7cde85bd1d263b71eea5
Fiona Carson <fionascarson@gmail.com>
2023-01-05 11:31:43 +0000

    creating_shiny_files

---
+++ group_project_dashboard/global.R
@@ -0,0 +1,22 @@
+# LOAD LIBRARIES
+
+# library()
+# library(xxxxx)
+
+
+
+
+
+# READ IN DATA
+
+# data <- read_csv("filepath.csv")
+
+
+
+
+
+# PREPARE PLOTS AND / OR TABLES
+
+
+
+# CREATE FUNCTIONS
