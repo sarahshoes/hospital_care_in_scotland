f5d142a1a6dd5d9aafee428e79db05eca1be12f3
Fiona Carson <fionascarson@gmail.com>
2023-01-10 13:23:50 +0000

    Update server.R

--- group_project_dashboard/server.R
+++ group_project_dashboard/server.R
@@ -28,38 +28,20 @@ server <- function(input, output) {
        group_by(date) %>% 
        summarise(percent_meeting_target_by_month = mean(percent_meeting_target)) %>% 
        
-       ggplot(aes(date, percent_meeting_target_by_month)) +
-       geom_line(color = "grey20") +
-       geom_hline(yintercept = 95, colour = "red", linetype = "dashed") + 
-       geom_hline(yintercept = 97, colour = "blue", linetype = "dashed") +
-       # use coord_cartesian to zoom in on y axis not scale_y_continuous() or ylim()
-       coord_cartesian(ylim=c(70,100)) +
-       theme_phs +
-       labs(title = "Percentage of Attendances Meeting 4 Hour Target",
-            subtitle = "Data Averaged by Month",
-            y = "% Meeting 4 Hour Target",
-            x = "Date") +
-       geom_rect(aes(xmin = ymd("2022-9-15"), xmax = ymd("2023-03-15"), 
-                     ymin = 0, ymax = Inf), fill = "skyblue", alpha = 0.01) +
-       geom_rect(aes(xmin = ymd("2021-9-15"), xmax = ymd("2022-03-15"), 
-                     ymin = 0, ymax = Inf), fill = "skyblue", alpha = 0.01) +
-       geom_rect(aes(xmin = ymd("2020-9-15"), xmax = ymd("2021-03-15"), 
-                     ymin = 0, ymax = Inf), fill = "skyblue", alpha = 0.01) +
-       geom_rect(aes(xmin = ymd("2020-01-01"), xmax = ymd("2020-03-15"), 
-                     ymin = 0, ymax = Inf), fill = "skyblue", alpha = 0.01) +
-       
+       timeseriesplot(aes(date, percent_meeting_target_by_month),
+                      "Percentage of Attendances Meeting 4 Hour Target", 
+                      "% Meeting 4 Hour Target") +
+       labs(subtitle = "Data Averaged By Month") +
        theme(axis.text = element_text(size = 12)) +
        # Make sure only whole percent number shows on y-axis (not e.g. 92.5%)
        scale_y_continuous(labels = scales::label_number(accuracy = 1)) +
        geom_hline(yintercept = 95, colour = "red", linetype = "dashed") + 
        geom_hline(yintercept = 97, colour = "blue", linetype = "dashed") +
-       annotate(geom = "text", x = as.Date("2022-08-01"), y = 94.2, 
+       annotate(geom = "text", x = as.Date("2022-08-01"), y = 94.5, 
                 label = "NHS 95% Target", colour = "red") +
-       annotate(geom = "text", x = as.Date("2022-08-01"), y = 96.2, 
+       annotate(geom = "text", x = as.Date("2022-08-01"), y = 96.5, 
                 label = "2018/2019 Average", colour = "blue")
    })
-   
-
 }
 
 
