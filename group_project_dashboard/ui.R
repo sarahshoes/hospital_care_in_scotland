c5ea603495bbd49091dabfda652b8b79a74672ce
Fiona Carson <fionascarson@gmail.com>
2023-01-10 08:18:09 +0000

    A working graph in shiny app!!

--- group_project_dashboard/ui.R
+++ group_project_dashboard/ui.R
@@ -12,7 +12,20 @@ ui <- fluidPage(
                 )
               )
             ),
-    tabPanel("Admissions"),
+    tabPanel("Admissions",
+             fluidRow(
+               column(width = 6,
+                      checkboxGroupInput(inputId = "minor_or_emerg_dept",
+                                         label = "Select Department Type",
+                                         choices = unique(waiting_times$department_type)
+                                         ),
+                      selectInput(inputId = "health_board",
+                                  label = "Select Health Board",
+                                  choices = unique(waiting_times$hb_name)
+                                  ),
+                      plotOutput("a_and_e_waiting_times")
+                      ))
+               ),
     tabPanel("Hospital Activity"),
     tabPanel("Discharge")
   
